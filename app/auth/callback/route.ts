99eeed0376ba5f49cbbcced6735b0203947e8827
iamianmoss <35110862+iamianmoss@users.noreply.github.com>
2025-08-27 18:57:28 -0400

    Create route.ts

---
+++ app/auth/callback/route.ts
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { sbServer } from '@/lib/supabase-server';
+
+export async function GET(req: NextRequest) {
+  const res = NextResponse.redirect(new URL('/', req.url));
+  const supabase = sbServer();
+
+  // This reads the code/hash in the URL and sets cookies
+  await supabase.auth.exchangeCodeForSession(req.url);
+  return res;
+}
