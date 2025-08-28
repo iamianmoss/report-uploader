d250d698f6f063f397a10976d2fd46b026c57867
iamianmoss <35110862+iamianmoss@users.noreply.github.com>
2025-08-28 14:12:10 -0400

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,6 +1,6 @@
-'use client';
+import { createBrowserClient } from '@supabase/ssr';
 
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
-import type { SupabaseClient } from '@supabase/supabase-js';
-
-export const supabase: SupabaseClient = createClientComponentClient();
+export const sb = createBrowserClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
