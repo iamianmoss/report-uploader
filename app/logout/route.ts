257a6d84bb49ce048089be5359dbf8bb8ad57167
iamianmoss <35110862+iamianmoss@users.noreply.github.com>
2025-08-27 21:59:00 -0400

    Update route.ts

--- app/logout/route.ts
+++ app/logout/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { sbServer } from '@/lib/supabase-server';
+import { sbServer } from '@/lib/supabase/server';
 
 export async function POST() {
   const supabase = sbServer();
